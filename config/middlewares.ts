ea763f189d0e4c3bfd4231f61029315a9df7fe7e
agnwoj99 <myvogue99@wp.pl>
2024-09-27 15:04:11 +0200

    Configured media storage

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,38 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "blob:",
+            "'self'",
+            "data:",
+            "cdn.jsdelivr.net",
+            "strapi.io",
+            process.env.SPACE_URL,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "strapi.io",
+            process.env.SPACE_URL,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
