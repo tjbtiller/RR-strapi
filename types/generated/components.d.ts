aa6b92b2f0d6d012dde7716e2defd7bdb70fe9eb
kactok <104381556+kactok@users.noreply.github.com>
2024-10-07 11:09:28 +0200

    Create About Us Page Collections

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -26,17 +26,6 @@ export interface HomepageCta extends Struct.ComponentSchema {
   };
 }
 
-export interface ColorHexColorHex extends Struct.ComponentSchema {
-  collectionName: 'components_color_hex_color_hexes';
-  info: {
-    displayName: 'ColorHex';
-    icon: 'brush';
-  };
-  attributes: {
-    Color: Schema.Attribute.String;
-  };
-}
-
 export interface ColorImageColorImage extends Struct.ComponentSchema {
   collectionName: 'components_color_image_color_images';
   info: {
@@ -48,13 +37,77 @@ export interface ColorImageColorImage extends Struct.ComponentSchema {
   };
 }
 
+export interface ColorHexColorHex extends Struct.ComponentSchema {
+  collectionName: 'components_color_hex_color_hexes';
+  info: {
+    displayName: 'ColorHex';
+    icon: 'brush';
+  };
+  attributes: {
+    Color: Schema.Attribute.String;
+  };
+}
+
+export interface AboutUsWhyUs extends Struct.ComponentSchema {
+  collectionName: 'components_about_us_whyuses';
+  info: {
+    displayName: 'FramedTextContentSection';
+    description: '';
+  };
+  attributes: {
+    Title: Schema.Attribute.String & Schema.Attribute.Required;
+    Tile: Schema.Attribute.Component<'about-us.tile', true>;
+  };
+}
+
+export interface AboutUsTile extends Struct.ComponentSchema {
+  collectionName: 'components_about_us_tiles';
+  info: {
+    displayName: 'Tile';
+  };
+  attributes: {
+    Image: Schema.Attribute.Media<'images'> & Schema.Attribute.Required;
+    Title: Schema.Attribute.String & Schema.Attribute.Required;
+    Text: Schema.Attribute.Text & Schema.Attribute.Required;
+  };
+}
+
+export interface AboutUsNumericalContent extends Struct.ComponentSchema {
+  collectionName: 'components_about_us_numerical_contents';
+  info: {
+    displayName: 'NumericalContentSection';
+    description: '';
+  };
+  attributes: {
+    Title: Schema.Attribute.String & Schema.Attribute.Required;
+    Text: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface AboutUsContentSection extends Struct.ComponentSchema {
+  collectionName: 'components_about_us_content_sections';
+  info: {
+    displayName: 'ImageTextContentSection';
+    description: '';
+  };
+  attributes: {
+    Title: Schema.Attribute.String & Schema.Attribute.Required;
+    Text: Schema.Attribute.Text & Schema.Attribute.Required;
+    Image: Schema.Attribute.Media<'images'> & Schema.Attribute.Required;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'homepage.hero-banner': HomepageHeroBanner;
       'homepage.cta': HomepageCta;
-      'color-hex.color-hex': ColorHexColorHex;
       'color-image.color-image': ColorImageColorImage;
+      'color-hex.color-hex': ColorHexColorHex;
+      'about-us.why-us': AboutUsWhyUs;
+      'about-us.tile': AboutUsTile;
+      'about-us.numerical-content': AboutUsNumericalContent;
+      'about-us.content-section': AboutUsContentSection;
     }
   }
 }
